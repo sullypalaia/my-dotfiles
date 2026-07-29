20d0520338ac7df624e1478cf02c1c81d9769b38
sullypalaia <sullypalaia@gmail.com>
2026-07-29 10:56:20 -0600

    add initial dotfiles

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim", 
+    name = "catppuccin-nvim", 
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+	transparent_background = true
+      })
+      vim.cmd.colorscheme("catppuccin-nvim")
+    end
+  }
+}
